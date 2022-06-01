f9f12f144088c67f60caed06c52301f95a87ba53
KelvinGKB <kengboonogoh@gmail.com>
2022-06-01 21:52:22 +0800

    Add tailwind primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  content: ['./src/**/*.tsx', './src/**/*.ts']
+  content: ['./src/**/*.tsx', './src/**/*.ts'],
+  theme: {
+    extend: {
+      colors: {
+        "primary":"#202020"
+      },
+    }
+  }
 }
